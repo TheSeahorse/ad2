ead253c168069c6b2ad8c35da2ae7a010c1c85f2
TheSeahorse <alexanderlahti@hotmail.com>
2020-12-03 17:19:44 +0100

    started on party_seating

--- assignment3/party_seating.py
+++ assignment3/party_seating.py
@@ -22,6 +22,7 @@ forever after.
 '''
 from src.party_seating_data import data  # noqa
 from typing import List, Tuple  # noqa
+from src.graph import Graph # noqa
 import unittest  # noqa
 
 # If your solution needs a queue, then you can use this one:
@@ -45,7 +46,64 @@ def party(known: List[List[int]]) -> Tuple[bool, List[int], List[int]]:
     Post:
     Ex:   party([[1, 2], [0], [0]]) = True, [0], [1, 2]
     """
-    return False, [], []
+    if not known:
+        return True, [], []
+    G = Graph(is_directed = False)
+    index = 0
+    for person in known:
+        for neighbor in person:
+            if not G.__contains__((str(index), str(neighbor))):
+                G.add_edge(str(index), str(neighbor))
+        index += 1
+
+    lonely_people = []
+    for r in range(0, len(known)):
+        if not str(r) in G.nodes:
+            lonely_people.append(str(r))
+
+    if not G.edges:
+        return True, [], list_str_to_int(lonely_people)
+
+    table1 = [G.edges[0][0]]
+    table2 = lonely_people
+    while(G.edges):
+        if not find_tables(G, G.edges[0][0], table1, table2):
+            return False, [], []
+    
+    
+    return True, list_str_to_int(table1), list_str_to_int(table2)
+
+
+def list_str_to_int(l: List[str]) -> List[int]:
+    """
+    Sig:  
+    Pre:
+    Post:
+    Ex:   
+    """
+    int_list = []
+    for i in l:
+        int_list.append(int(i))
+    return int_list
+
+
+def find_tables(G: Graph, node: str, table1: List[str], table2: List[str]) -> bool:
+    """
+    Sig:  List[List[int]] -> Tuple[bool, List[int], List[int]]
+    Pre: node exists in table1
+    Post:
+    Ex:   party([[1, 2], [0], [0]]) = True, [0], [1, 2]
+    """
+    if not G.neighbors(node):
+        return True
+    for n in G.neighbors(node):
+        if n in table2:
+            return False
+        G.remove_edge(n, node)
+        table2.append(n)
+        if not find_tables(G, n, table2, table1):
+            return False
+    return True
 
 
 class PartySeatingTest(unittest.TestCase):
@@ -91,15 +149,21 @@ class PartySeatingTest(unittest.TestCase):
         self.known_test(known, A, B)
 
     def test_party(self):
+        counter = 0
         for instance in data:
             known = instance["known"]
             expected = instance["expected"]
             success, A, B = party(known)
+            print("counter: " + str(counter))
+            print("known: " + str(known))
+            print("expected: " + str(expected))
+            print("our result: " + str(success) + "\ntable1: " + str(A), "\ntable2: " + str(B))
 
             if not expected:
                 self.assertFalse(success)
                 continue
             self.known_test(known, A, B)
+            counter += 1
 
 
 if __name__ == '__main__':
