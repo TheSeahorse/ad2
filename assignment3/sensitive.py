bbb4edc652678792ff101259f9606a727f6262cc
TheSeahorse <alexanderlahti@hotmail.com>
2021-01-14 16:13:11 +0100

    assignment 3 done

--- assignment3/sensitive.py
+++ assignment3/sensitive.py
@@ -47,8 +47,54 @@ def sensitive(G: Graph, s: str, t: str) -> Tuple[str, str]:
     Post:
     Ex:   sensitive(g1, 'a', 'f') = ('b', 'd')
     """
-    return None, None
+    edges = G.edges
+    nodes = G.nodes
 
+    R = Graph(is_directed=True) 
+
+    for (u, v) in edges:
+        flow = G.flow(u, v)
+        capacity = G.capacity(u, v)
+        if capacity == flow:
+            R.add_edge(v, u)
+        elif flow == 0:
+            R.add_edge(u, v)
+        else:
+            R.add_edge(u, v)
+            R.add_edge(v, u)
+
+    source_side = []
+    sink_side = []
+    source_side.append(s)
+
+    find_source_side(R, s, source_side)
+
+    for node in nodes:
+        if not node in source_side:
+            sink_side.append(node)
+
+    for s in source_side:
+        for t in sink_side:
+            if (s, t) in edges:
+                return (s, t)
+
+    return (None, None)
+
+
+def find_source_side(R: Graph, node: str, source_side: list):
+    neighbors = R.neighbors(node)
+    for neighbor in neighbors:
+        if not neighbor in source_side:
+            source_side.append(neighbor)
+            find_source_side(R, neighbor, source_side)
+
+
+
+class SensitiveTest(unittest.TestCase):
+    """
+    Test suite for the sensitive edge problem
+    """
+    logger = logging.getLogger('SensitiveTest')
 
 class SensitiveTest(unittest.TestCase):
     """
@@ -68,19 +114,30 @@ class SensitiveTest(unittest.TestCase):
         g1.add_edge('d', 'f', capacity=20, flow=19)
         g1.add_edge('e', 'd', capacity=7, flow=7)
         g1.add_edge('e', 'f', capacity=4, flow=4)
-        self.assertEqual(
+        self.assertIn(
             sensitive(g1, 'a', 'f'),
-            ('b', 'd')
+            [('b', 'd'), ('e', 'd'), ('e', 'f')]
         )
         g2 = Graph(is_directed=True)
         g2.add_edge('a', 'b', capacity=1, flow=1)
         g2.add_edge('a', 'c', capacity=100, flow=4)
         g2.add_edge('b', 'c', capacity=100, flow=1)
         g2.add_edge('c', 'd', capacity=5, flow=5)
-        self.assertIn(
+        self.assertEqual(
             sensitive(g2, 'a', 'd'),
-            set([('a', 'c'), ('c', 'd')])
+            ('c', 'd')
         )
+
+    def test_sensitive(self):
+        for instance in data:
+            graph = instance['digraph'].copy()
+            u, v = sensitive(graph, instance["source"], instance["sink"])
+            self.assertIn(u, graph, f"Invalid edge ({u}, {v})")
+            self.assertIn((u, v), graph, f"Invalid edge ({u}, {v})")
+            self.assertIn(
+                (u, v),
+                instance["sensitive_edges"]
+            )
 
     def test_sensitive(self):
         for instance in data:
